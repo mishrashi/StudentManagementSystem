5d0ad591d4e60c36189e80dc7d24baa2e930883f
Shivansh Kumar Mishra <105987249+mishrashi@users.noreply.github.com>
2023-05-20 22:22:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Create from './Create';
+
+import Student from './Student';
+
+import Update from './Update';
+
+
+
+function App() { 
+   return (   
+   <div className="App">      
+<BrowserRouter>    
+    <Routes>          
+      <Route path='/' element={<Student />}>
+        </Route>          
+        <Route path='/create' element={<Create />}></Route>       
+           <Route path='/update/:id' element={<Update />}></Route>   
+                </Routes>      
+                </BrowserRouter>   
+                 </div>  
+    );}
+
+
+export default App;
+
